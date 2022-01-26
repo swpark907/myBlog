fee189adf4f13e26d6955648b8a410115ccfa14a
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-01-26 09:56:33 +0900

    add base components & setting default layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { PostCard, Categories, PostWidget} from '../components';
 
 const posts = [
-  {title: 'React Testing', excerpt: 'Learn React Testing'},
-  {title: 'React with Tailwind', excerpt: 'Learn React with Tailwind'},
-]
+  { title: "React Testing", excerpt: "Learn React Testing" },
+  { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
+];
 
 export default function Home() {
   return (
@@ -12,15 +13,20 @@ export default function Home() {
         <title>MY_BLOG</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        {posts.map((post, index) => 
-          (<div>
-            {post.title}
-            {post.excerpt}
-          </div>)
-        )}
+
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) => (
+            <PostCard post={post} key={post.title}></PostCard>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
       </div>
-      
     </div>
-  )
+  );
 }
